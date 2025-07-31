039e190cd15e4ccac4eb35da688f64e98d9c72d1
Tamirys Hariadny Camargo Couto <tamirys.couto@escola.pr.gov.br>
2025-07-31 12:17:40 +0000

    Começando o jogo de adivinhação

---
+++ jogo.py
@@ -0,0 +1,10 @@
+import random
+print("****************************")
+print("*    Boas vindas ao        *")
+print("*   Jogo de adivinhação🎉  *")
+print("*         Tammys💙         *")
+print("****************************")
+
+numeroSecreto= random.randrange(0,100)
+totaldeTentativas = 0
+pontos = 100
